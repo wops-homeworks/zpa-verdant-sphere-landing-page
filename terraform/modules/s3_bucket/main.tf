65d6a560a5bba61988c2a8a62fc4d487d2e79ede
Zaw Phyo Aung <snippyjustro@gmail.com>
2024-09-24 15:45:05 +0700

    terraform provision s3 6

--- terraform/modules/s3_bucket/main.tf
+++ terraform/modules/s3_bucket/main.tf
@@ -6,6 +6,16 @@ resource "aws_s3_bucket" "this" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   bucket = aws_s3_bucket.this.id
 
@@ -13,5 +23,39 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_public" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.allow_access_from_public_policy.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_public_policy" {
+
+  statement {
+    sid = "AllowPublicRead"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+    ]
+    resources = [
+      aws_s3_bucket.this.arn,
+      "${aws_s3_bucket.this.arn}/*",
+    ]
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "website-config" {
+  bucket = aws_s3_bucket.this.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "404.jpeg"
   }
 }
